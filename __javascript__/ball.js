b5380ca7baa34a4261bf8f8a99d984eaf697e428
Stephen Theodore <stevet@Stephens-MBP.domain>
2017-05-24 00:49:01 -0700

    bullets in, not working right

--- __javascript__/ball.js
+++ __javascript__/ball.js
@@ -1,5 +1,5 @@
 "use strict";
-// Transcrypt'ed from Python, 2017-05-21 14:24:20
+// Transcrypt'ed from Python, 2017-05-24 00:48:16
 function ball () {
    var __symbols__ = ['__py3.6__', '__esv6__'];
     var __all__ = {};
@@ -3231,6 +3231,22 @@ function ball () {
 		var random = {};
 		var three =  __init__ (__world__.org.threejs);
 		__nest__ (random, '', __init__ (__world__.random));
+		var wrap = function (obj) {
+			var pos = obj.position;
+			if (pos.x < -(30)) {
+				pos.x = 30;
+			}
+			else if (pos.x > 30) {
+				pos.x = -(30);
+			}
+			if (pos.y < -(30)) {
+				pos.y = 30;
+			}
+			else if (pos.y > 30) {
+				pos.y = -(30);
+			}
+			obj.matrixWorld.setPosition (pos);
+		};
 		var clamp = function (val, low, high) {
 			return max (min (val, high), low);
 		};
@@ -3273,15 +3289,21 @@ function ball () {
 				for (var [_, eachhandler] of self.handlers.py_items ()) {
 					eachhandler.py_update (self, interval);
 				}
+			});},
+			get py_clear () {return __get__ (this, function (self, axis) {
+				self.handlers.py_get (axis).value = 0;
 			});}
 		});
 		var ControlAxis = __class__ ('ControlAxis', [object], {
-			get __init__ () {return __get__ (this, function (self, positive_key, negative_key, attack, decay) {
+			get __init__ () {return __get__ (this, function (self, positive_key, negative_key, attack, decay, deadzone) {
 				if (typeof attack == 'undefined' || (attack != null && attack .hasOwnProperty ("__kwargtrans__"))) {;
 					var attack = 1;
 				};
 				if (typeof decay == 'undefined' || (decay != null && decay .hasOwnProperty ("__kwargtrans__"))) {;
 					var decay = 0;
+				};
+				if (typeof deadzone == 'undefined' || (deadzone != null && deadzone .hasOwnProperty ("__kwargtrans__"))) {;
+					var deadzone = 0.02;
 				};
 				if (arguments.length) {
 					var __ilastarg0__ = arguments.length - 1;
@@ -3294,6 +3316,7 @@ function ball () {
 								case 'negative_key': var negative_key = __allkwargs0__ [__attrib0__]; break;
 								case 'attack': var attack = __allkwargs0__ [__attrib0__]; break;
 								case 'decay': var decay = __allkwargs0__ [__attrib0__]; break;
+								case 'deadzone': var deadzone = __allkwargs0__ [__attrib0__]; break;
 							}
 						}
 					}
@@ -3304,17 +3327,26 @@ function ball () {
 				self.negative = negative_key;
 				self.attack = attack;
 				self.decay = decay;
+				self.deadzone = deadzone;
 				self.value = 0;
 			});},
 			get py_update () {return __get__ (this, function (self, keyboard, interval) {
 				self.value -= (interval * self.decay) * self.value;
+				var dz = abs (self.value) < self.deadzone;
 				if (keyboard.py_get (self.positive)) {
+					var dz = false;
 					self.value += interval * self.attack;
 				}
 				if (keyboard.py_get (self.negative)) {
+					var dz = false;
 					self.value -= interval * self.attack;
 				}
-				self.value = clamp (self.value, -(1), 1);
+				if (dz) {
+					self.value = 0;
+				}
+				else {
+					self.value = clamp (self.value, -(1), 1);
+				}
 			});}
 		});
 		var WIDTH = window.innerWidth;
@@ -3325,8 +3357,94 @@ function ball () {
 		var scene = null;
 		var last_frame = new Date;
 		var kb = Keyboard ();
-		kb.add_handler ('spin', ControlAxis ('s', 'a', __kwargtrans__ ({attack: 1, decay: 0.49})));
-		var RX = 0;
+		kb.add_handler ('spin', ControlAxis ('s', 'a', __kwargtrans__ ({attack: 1, decay: 0.6})));
+		kb.add_handler ('thrust', ControlAxis ('w', 'z', __kwargtrans__ ({attack: 0.75, decay: 2, deadzone: 0.1})));
+		kb.add_handler ('fire', ControlAxis ('q', 'b', __kwargtrans__ ({attack: 1})));
+		var Bullet = __class__ ('Bullet', [object], {
+			EXPIRES: 1.5,
+			RESET_POS: three.Vector3 (0, 0, 1000),
+			BULLET_SPEED: 12,
+			BULLET_POINTER: 0,
+			BULLETS: list ([]),
+			get __init__ () {return __get__ (this, function (self) {
+				self.vector = three.Vector3 (0, 0, 0);
+				self.geo = three.Mesh (three.BoxGeometry (1, 1, 1), three.MeshBasicMaterial (dict ({'color': 16777215})));
+				self.lifespan = 0;
+				self.reset ();
+			});},
+			get py_update () {return __get__ (this, function (self, t) {
+				if (self.geo.visible) {
+					self.lifespan += t;
+					if (self.lifespan > self.EXPIRES) {
+						self.reset ();
+						return ;
+					}
+				}
+			});},
+			get reset () {return __get__ (this, function (self) {
+				self.geo.visible = false;
+				self.geo.matrixWorld.setPosition (self.RESET_POS);
+			});}
+		});
+		var make_bullets = function (scene, amount) {
+			Bullet.BULLETS = function () {
+				var __accu0__ = [];
+				for (var n = 0; n < amount; n++) {
+					__accu0__.append (Bullet ());
+				}
+				return __accu0__;
+			} ();
+			for (var b of Bullet.BULLETS) {
+				scene.add (b.geo);
+			}
+		};
+		var fire = function (pos, vector) {
+			for (var eachbullet of Bullet.BULLETS) {
+				if (!(eachbullet.geo.visible)) {
+					eachbullet.geo.matrixWorld.setPosition (pos);
+					eachbullet.vector = vector;
+					eachbullet.lifespan = 0;
+					eachbullet.geo.visible = true;
+					return ;
+				}
+			}
+			print ('click');
+		};
+		var Ship = __class__ ('Ship', [object], {
+			ROTATE_SPEED: 2.1,
+			THRUST: 0.075,
+			get __init__ () {return __get__ (this, function (self) {
+				self.geo = three.Mesh (three.BoxGeometry (2, 3, 2), three.MeshNormalMaterial ());
+				var exhaust = three.Mesh (three.BoxGeometry (1, 2, 1), three.MeshBasicMaterial (dict ({'color': 16776960})));
+				self.geo.add (exhaust);
+				exhaust.translateY (-(2));
+				self.exhaust = exhaust;
+				self.momentum = three.Vector3 (0, 0, 0);
+			});},
+			get add () {return __get__ (this, function (self, scn) {
+				scn.add (self.geo);
+			});},
+			get py_update () {return __get__ (this, function (self, t) {
+				var thrust = kb.get_axis ('thrust');
+				self.geo.rotateZ (((kb.get_axis ('spin') * self.ROTATE_SPEED) * t) * -(1));
+				if (thrust > 0) {
+					var thrust_amt = (thrust * self.THRUST) * t;
+					self.momentum = self.momentum.add (self.heading.multiplyScalar (thrust_amt));
+				}
+				var current_pos = self.geo.position;
+				self.geo.matrixWorld.setPosition (current_pos.add (self.momentum));
+				self.exhaust.visible = thrust > 0;
+				if (kb.get_axis ('fire') == 1) {
+					fire (self.geo.position, self.heading);
+					print ('fire', self.geo.position.x, self.geo.position.y);
+					kb.py_clear ('fire');
+				}
+			});},
+			get get_heading () {return __get__ (this, function (self) {
+				return three.Vector3 (self.geo.matrixWorld.elements [4], self.geo.matrixWorld.elements [5], self.geo.matrixWorld.elements [6]);
+			});}
+		});
+		Object.defineProperty (Ship, 'heading', property.call (Ship, Ship.get_heading));;
 		var Asteroid = __class__ ('Asteroid', [object], {
 			get __init__ () {return __get__ (this, function (self) {
 				self.geo = three.Mesh (three.SphereGeometry (random.randint (1, 3)), three.MeshNormalMaterial ());
@@ -3348,6 +3466,7 @@ function ball () {
 			}
 			return __accu0__;
 		} ();
+		var ship = Ship ();
 		var init = function () {
 			scene = three.Scene ();
 			camera = three.PerspectiveCamera (70, WIDTH / HEIGHT, 1, 500);
@@ -3355,11 +3474,11 @@ function ball () {
 			camera.lookAt (scene.position);
 			renderer = three.WebGLRenderer (dict ({'Antialias': true}));
 			renderer.setSize (WIDTH, HEIGHT);
-			cube = three.Mesh (three.BoxGeometry (2, 2, 2), three.MeshNormalMaterial ());
-			scene.add (cube);
+			ship.add (scene);
 			for (var item of asteroids) {
 				item.add (scene);
 			}
+			make_bullets (scene, 8);
 			document.getElementById ('XX').appendChild (renderer.domElement);
 			document.onkeydown = kb.key_down;
 			document.onkeyup = kb.key_up;
@@ -3368,14 +3487,16 @@ function ball () {
 			requestAnimationFrame (render);
 			var t = (new Date - last_frame) / 1000.0;
 			kb.py_update (t);
-			var SPEED = 4;
-			cube.position.y = kb.get_axis ('spin') * 45;
-			cube.rotation.y -= (SPEED * t) * 0.3;
+			for (var b of Bullet.BULLETS) {
+				b.py_update (t);
+			}
+			ship.py_update (t);
+			wrap (ship.geo);
 			for (var item of asteroids) {
 				item.py_update (t);
+				wrap (item.geo);
 			}
 			renderer.render (scene, camera);
-			set_element ('ZZ', t * 1000);
 			last_frame = new Date;
 		};
 		init ();
@@ -3386,24 +3507,29 @@ function ball () {
 		'</use>')
 		__pragma__ ('<all>')
 			__all__.Asteroid = Asteroid;
+			__all__.Bullet = Bullet;
 			__all__.ControlAxis = ControlAxis;
 			__all__.HEIGHT = HEIGHT;
 			__all__.Keyboard = Keyboard;
-			__all__.RX = RX;
+			__all__.Ship = Ship;
 			__all__.WIDTH = WIDTH;
 			__all__.asteroids = asteroids;
 			__all__.camera = camera;
 			__all__.clamp = clamp;
 			__all__.cube = cube;
+			__all__.fire = fire;
 			__all__.init = init;
 			__all__.kb = kb;
 			__all__.last_frame = last_frame;
+			__all__.make_bullets = make_bullets;
 			__all__.now = now;
 			__all__.render = render;
 			__all__.renderer = renderer;
 			__all__.scene = scene;
 			__all__.set_element = set_element;
+			__all__.ship = ship;
 			__all__.sign = sign;
+			__all__.wrap = wrap;
 		__pragma__ ('</all>')
 	}) ();
    return __all__;
--- ball.py
+++ ball.py
@@ -2,9 +2,25 @@ import org.threejs as three
 import random
 
 
+def wrap(obj: three.Object3d):
+    pos = obj.position
+
+    if pos.x < -30:
+        pos.x = 30
+    elif pos.x > 30:
+        pos.x = -30
+
+    if pos.y < -30:
+        pos.y = 30
+    elif pos.y > 30:
+        pos.y = -30
+
+    obj.matrixWorld.setPosition(pos)
+
 
 def clamp(val, low, high):
     return max(min(val, high), low)
+
 
 def sign(val):
     if val > 0:
@@ -12,6 +28,7 @@ def sign(val):
     if val < 0:
         return -1
     return 0
+
 
 def now():
     return __new__(Date)
@@ -22,9 +39,8 @@ def set_element(id, value):
 
 
 class Keyboard:
-
     def __init__(self):
-        self.keyboard = {0:False}
+        self.keyboard = {0: False}
         self.handlers = {}
 
     def key_down(self, key):
@@ -42,31 +58,41 @@ class Keyboard:
     def add_handler(self, name, handler):
         self.handlers[name] = handler
 
-    def update (self, interval):
+    def update(self, interval):
         for _, eachhandler in self.handlers.items():
             eachhandler.update(self, interval)
 
+    def clear(self, axis):
+        self.handlers.get(axis).value = 0
+
 
 class ControlAxis:
-
     __pragma__('kwargs')
-    def __init__(self, positive_key, negative_key, attack=1, decay=0):
+
+    def __init__(self, positive_key: str, negative_key: str, attack=1, decay=0, deadzone=0.02):
         self.positive = positive_key
         self.negative = negative_key
         self.attack = attack
         self.decay = decay
+        self.deadzone = deadzone
         self.value = 0
+
     __pragma__('nokwargs')
 
-    def update(self, keyboard, interval):
+    def update(self, keyboard: Keyboard, interval: float):
         self.value -= (interval * self.decay * self.value)
+        dz = abs(self.value) < self.deadzone
         if keyboard.get(self.positive):
+            dz = False
             self.value += interval * self.attack
         if keyboard.get(self.negative):
+            dz = False
             self.value -= interval * self.attack
-        self.value = clamp(self.value, -1, 1)
 
-
+        if dz:
+            self.value = 0
+        else:
+            self.value = clamp(self.value, -1, 1)
 
 
 WIDTH = window.innerWidth
@@ -77,42 +103,127 @@ cube = None
 scene = None
 last_frame = __new__(Date)
 kb = Keyboard()
-kb.add_handler('spin', ControlAxis('s', 'a', attack = 1, decay= 0.49))
+kb.add_handler('spin', ControlAxis('s', 'a', attack=1, decay=.6))
+kb.add_handler('thrust', ControlAxis('w', 'z', attack=.75, decay=2, deadzone=.1))
+kb.add_handler('fire', ControlAxis('q', 'b', attack=1))
 
 
+class Bullet:
 
-RX = 0
+    EXPIRES = 1.5
+    RESET_POS = three.Vector3(0, 0, 1000)
+    BULLET_SPEED = 12
+    BULLET_POINTER = 0
+    BULLETS = []
+
+    def __init__(self):
+        self.vector = three.Vector3(0,0,0)
+        self.geo = three.Mesh(three.BoxGeometry(1, 1, 1),
+                              three.MeshBasicMaterial({'color': 0xffffff}))
+        self.lifespan = 0
+        self.reset()
+
+    def update(self, t):
+        if self.geo.visible:
+            self.lifespan += t
+            if self.lifespan > self.EXPIRES:
+                self.reset()
+                return
+            #momentum  = self.vector.multiplyScalar(self.BULLET_SPEED * t)
+            #current_pos = self.geo.position
+            #self.geo.matrixWorld.setPosition(current_pos.add(momentum))
+
+    def reset(self):
+        self.geo.visible = False
+        self.geo.matrixWorld.setPosition(self.RESET_POS)
 
 
+def make_bullets(scene, amount):
+    Bullet.BULLETS = [Bullet() for n in range(amount)]
+    for b in Bullet.BULLETS:
+        scene.add(b.geo)
 
 
+def fire(pos, vector):
+    for eachbullet in Bullet.BULLETS:
+        if not eachbullet.geo.visible:
+            eachbullet.geo.matrixWorld.setPosition(pos)
+            eachbullet.vector = vector
+            eachbullet.lifespan = 0
+            eachbullet.geo.visible = True
 
-class Asteroid:
+            return
+    print ("click")
+
+class Ship:
+    ROTATE_SPEED = 2.1
+    THRUST = .075
 
     def __init__(self):
         self.geo = three.Mesh(
-            three.SphereGeometry(random.randint(1,3)),
+            three.BoxGeometry(2, 3, 2),
+            three.MeshNormalMaterial()
+        )
+        exhaust = three.Mesh(
+            three.BoxGeometry(1, 2, 1),
+            three.MeshBasicMaterial({'color': 0xffff00})
+        )
+        self.geo.add(exhaust)
+        exhaust.translateY(-2)
+        self.exhaust = exhaust
+        self.momentum = three.Vector3(0, 0, 0)
+
+    def add(self, scn: three.Scene):
+        scn.add(self.geo)
+
+    def update(self, t):
+        thrust = kb.get_axis('thrust')
+        self.geo.rotateZ(kb.get_axis('spin') * self.ROTATE_SPEED * t * -1)
+
+        if thrust > 0:
+            thrust_amt = thrust * self.THRUST * t
+            self.momentum = self.momentum.add(self.heading.multiplyScalar(thrust_amt))
+
+        current_pos = self.geo.position
+        self.geo.matrixWorld.setPosition(current_pos.add(self.momentum))
+        self.exhaust.visible = thrust > 0
+
+        if kb.get_axis('fire') == 1:
+            fire(self.geo.position, self.heading)
+            print("fire", self.geo.position.x, self.geo.position.y)
+            kb.clear('fire')
+
+    def get_heading(self) -> float:
+        # return the local Y axis, since Z is 'up'
+        return three.Vector3(self.geo.matrixWorld.elements[4],
+                             self.geo.matrixWorld.elements[5],
+                             self.geo.matrixWorld.elements[6])
+
+    heading = property(get_heading)
+
+
+class Asteroid:
+    def __init__(self):
+        self.geo = three.Mesh(
+            three.SphereGeometry(random.randint(1, 3)),
             three.MeshNormalMaterial()
         )
         self.geo.position.set(random.random() * 10, random.random() * 10, 0)
-        self.momentum = three.Vector3(random.random() - 0.5, random.random() - 0.5, random.random()-0.5)
+        self.momentum = three.Vector3(random.random() - 0.5, random.random() - 0.5, random.random() - 0.5)
         self.momentum.multiplyScalar(3)
 
-
-    def add(self, scene):
+    def add(self, scene: three.Scene):
         scene.add(self.geo)
 
     def update(self, t):
-
-
         self.geo.translateOnAxis(self.momentum, t)
 
 
 asteroids = [Asteroid() for a in range(6)]
+ship = Ship()
+
 
 def init():
-
-
     global renderer
     global camera
     global cube
@@ -125,45 +236,41 @@ def init():
     renderer = three.WebGLRenderer({'Antialias': True})
     renderer.setSize(WIDTH, HEIGHT)
 
-    cube = three.Mesh(
-        three.BoxGeometry(2, 2, 2),
-        three.MeshNormalMaterial()
-    )
-    scene.add(cube)
-
-
+    ship.add(scene)
     for item in asteroids:
         item.add(scene)
+
+    make_bullets(scene, 8)
 
     document.getElementById("XX").appendChild(renderer.domElement)
     document.onkeydown = kb.key_down
     document.onkeyup = kb.key_up
 
+
 def render():
-    #nonlocal  RX
     global scene
     global cube
     global renderer
     global last_frame
+    global VELOCITY
+
     requestAnimationFrame(render)
 
     t = (__new__(Date) - last_frame) / 1000.0
     kb.update(t)
 
-    SPEED = 4
-    cube.position.y = kb.get_axis('spin') * 45
-    cube.rotation.y -= SPEED * t * .3
+    for b in Bullet.BULLETS:
+        b.update(t)
 
+    ship.update(t)
+    wrap(ship.geo)
     for item in asteroids:
         item.update(t)
+        wrap(item.geo)
 
     renderer.render(scene, camera)
-    set_element("ZZ", t * 1000)
     last_frame = __new__(Date)
-
 
 
 init()
 render()
-
-# engine = Game()
