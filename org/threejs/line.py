5d587907898e2b59878cb4a58ac8b84eab46c0c5
theodox <steve@theodox.com>
2017-05-26 22:10:10 -0700

    renames and cleanups

--- org/threejs/line.py
+++
@@ -1,8 +0,0 @@
-import org.threejs as three
-
-def make_line(color, *points):
-    geo = three.Geometry()
-    for p in points:
-        geo.vertices.push(three.Vector3(*p))
-    mtl = three.LineBasicMaterial({'color': color})
-    return three.Line(geo, mtl)
